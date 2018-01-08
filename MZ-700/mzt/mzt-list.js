5346f4aadf5c774d7c52341b1d32fa90625f918d
Koji Takami <vzg03566@gmail.com>
2018-01-08 11:42:49 +0900

    Add more MZTs

---
+++ MZ-700/mzt/mzt-list.js
@@ -0,0 +1,10 @@
+loadableMZT([
+    { path: "WH_newmon",            name: "Wonder House",       mz700_buttonStyle: { color: 2, backgroundColor: 6 } },
+    { path: "BdHopper",             name: "Building Hopper",    mz700_buttonStyle: { color: 7, backgroundColor: 1 } },
+    { path: "zeplis/GENOSS_700",    name: "GENOSS",             mz700_buttonStyle: { color: 7, backgroundColor: 1 } },
+    { path: "zeplis/ZELBUS",        name: "ZELBUS",             mz700_buttonStyle: { color: 7, backgroundColor: 0 } },
+    { path: "zeplis/ZELDIS",        name: "ZELDIS",             mz700_buttonStyle: { color: 4, backgroundColor: 0 } },
+    { path: "zeplis/ZEPLIS",        name: "ZEPLIS",             mz700_buttonStyle: { color: 4, backgroundColor: 0 } },
+    { path: "zeplis/ZEPLIS2",       name: "ZEPLIS2",            mz700_buttonStyle: { color: 7, backgroundColor: 0 } },
+    { path: "zeplis/ZEPLIS3",       name: "ZEPLIS3",            mz700_buttonStyle: { color: 3, backgroundColor: 0 } }
+]);
